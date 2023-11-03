f8be380bd2108e72ca6c0a767b967a0b5db5c010
Terence Ponce <terence@dashfloat.com>
2023-11-03 16:52:24 +0000

    Install flowbite

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -9,7 +9,8 @@ module.exports = {
   content: [
     "./js/**/*.js",
     "../lib/dashfloat_web.ex",
-    "../lib/dashfloat_web/**/*.*ex"
+    "../lib/dashfloat_web/**/*.*ex",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {
@@ -19,6 +20,7 @@ module.exports = {
     },
   },
   plugins: [
+    require("flowbite/plugin"),
     require("@tailwindcss/forms"),
     // Allows prefixing tailwind classes with LiveView classes to add rules
     // only when LiveView classes are applied, for example:
