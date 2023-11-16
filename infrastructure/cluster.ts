051fd07a62432792e699a48ab0a4ebe60484e8a1
Terence Ponce <terence@dashfloat.com>
2023-11-16 17:44:08 +0000

    Convert tab to space

--- infrastructure/cluster.ts
+++ infrastructure/cluster.ts
@@ -3,47 +3,47 @@ import * as gcp from "@pulumi/gcp";
 import * as k8s from "@pulumi/kubernetes";
 
 import {
-	clusterName,
-	clusterNodeCount,
-	clusterNodeMachineType,
+  clusterName,
+  clusterNodeCount,
+  clusterNodeMachineType,
 } from "./config";
 
 const engineVersion = gcp.container.getEngineVersions().then(v => v.latestMasterVersion);
 
 export const cluster = new gcp.container.Cluster(clusterName, {
-	// We can't create a cluster with no node pool defined, but we want to only use
-	// separately managed node pools. So we create the smallest possible default
-	// node pool and immediately delete it.
-	initialNodeCount: 1,
-	removeDefaultNodePool: true,
-	minMasterVersion: engineVersion,
+  // We can't create a cluster with no node pool defined, but we want to only use
+  // separately managed node pools. So we create the smallest possible default
+  // node pool and immediately delete it.
+  initialNodeCount: 1,
+  removeDefaultNodePool: true,
+  minMasterVersion: engineVersion,
 });
 
 const nodePool = new gcp.container.NodePool("primary-node-pool", {
-	cluster: cluster.name,
-	initialNodeCount: clusterNodeCount,
-	location: cluster.location,
-	nodeConfig: {
-		preemptible: true,
-		machineType: clusterNodeMachineType,
-		oauthScopes: [
-			"https://www.googleapis.com/auth/compute",
-			"https://www.googleapis.com/auth/devstorage.read_only",
-			"https://www.googleapis.com/auth/logging.write",
-			"https://www.googleapis.com/auth/monitoring",
-		],
-	},
-	version: engineVersion,
-	management: {
-		autoRepair: true,
-	},
+  cluster: cluster.name,
+  initialNodeCount: clusterNodeCount,
+  location: cluster.location,
+  nodeConfig: {
+    preemptible: true,
+    machineType: clusterNodeMachineType,
+    oauthScopes: [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ],
+  },
+  version: engineVersion,
+  management: {
+    autoRepair: true,
+  },
 }, {
-	dependsOn: [cluster],
+  dependsOn: [cluster],
 });
 
 export const config = pulumi.
-	all([ cluster.name, cluster.endpoint, cluster.masterAuth ]).
-	apply(([ name, endpoint, auth ]) => {
+  all([ cluster.name, cluster.endpoint, cluster.masterAuth ]).
+  apply(([ name, endpoint, auth ]) => {
     const context = `${gcp.config.project}_${gcp.config.zone}_${name}`;
     return `apiVersion: v1
 clusters:
