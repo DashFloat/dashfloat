538995623c2096157d51d2b4427c251b70739c30
Terence Ponce <terence@dashfloat.com>
2023-11-16 17:39:59 +0000

    Add production config

--- infrastructure/config.ts
+++ infrastructure/config.ts
@@ -5,5 +5,3 @@ const config = new Config();
 export const clusterName = config.require("clusterName");
 export const clusterNodeCount = config.getNumber("clusterNodeCount") || 1;
 export const clusterNodeMachineType = config.get("clusterNodeMachineType") || "n1-standard-1";
-export const clusterUsername = config.requireSecret("clusterUsername")
-export const clusterPassword = config.requireSecret("clusterPassword");
